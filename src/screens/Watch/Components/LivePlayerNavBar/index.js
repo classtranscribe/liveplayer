c1a771e95927b3f544051d1c0b9259705c4003fc
Lawrence Angrave <angrave@illinois.edu>
2022-03-02 13:23:14 -0600

    linting

--- src/screens/Watch/Components/LivePlayerNavBar/index.js
+++ src/screens/Watch/Components/LivePlayerNavBar/index.js
@@ -5,19 +5,19 @@ import { UISlogo } from '../../../../assets/images';
 export function LivePlayerNavBar() {
   return (
     <Navbar bg="light">
-      <Navbar.Brand href="https://www.bot.uillinois.edu/">
-        <img
-          src={UISlogo}
-          width="30"
-          height="42"
-          alt="Board of Trustees"
-        />
-        Board of Trustees
+        <Navbar.Brand href="https://www.bot.uillinois.edu/">
+          <img
+            src={UISlogo}
+            width="30"
+            height="42"
+            alt="Board of Trustees"
+          />
+          Board of Trustees
       </Navbar.Brand>
       <Navbar.Collapse className="justify-content-end">
-        <Navbar.Text>
-          <a href="https://www.uis.edu/" style={{ textDecoration: 'none' }}>University of Illinois Springfield </a>
-        </Navbar.Text>
+          <Navbar.Text>
+            <a href="https://www.uis.edu/" style={{ textDecoration: 'none' }}>University of Illinois Springfield </a>
+          </Navbar.Text>
       </Navbar.Collapse>
     </Navbar>
     );
